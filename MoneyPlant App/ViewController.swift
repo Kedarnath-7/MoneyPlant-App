5a8628d8d158a14fa7f99d848b68f105940b27f2
Kedarnath-7 <kedarnath7218gtl@gmail.com>
2024-11-03 21:29:47 +0530

    Initial Commit

---
+++ MoneyPlant App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyPlant App
+//
+//  Created by admin86 on 02/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
